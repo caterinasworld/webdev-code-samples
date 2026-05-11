6124432ca8533d06e0f9b5f49b90a0a2df350df7
Caterina Paun <caterina@pdx.edu>
2026-05-11 16:22:54 -0700

    Update: hello world javascript example

--- 06-javascript/01-hello-world.js
+++ 06-javascript/01-hello-world.js
@@ -2,21 +2,21 @@
   console.log('\n========== JavaScript Hello World ==========\n');
   console.log('Hello there 👋');
 
-  // Can we call printDate() on line 5?
+  // Can we call printDate() on line 6?
   // printDate();
 
   function printDate() {
     const today = new Date();
-    console.log(`Today's date is ${today}`);
+    console.log(`FUNCTION 1 -- Today's date is ${today}`);
   }
 
   printDate();
 
-  // Can we call todaysDate() on line 15?
+  // Can we call todaysDate() on line 16?
   // todaysDate();
 
   const todaysDate = function printTodaysDate() {
-    console.log(`Today's date is ${new Date()}`);
+    console.log(`FUNCTION 2 -- Today's date is ${new Date()}`);
   };
 
   todaysDate();
